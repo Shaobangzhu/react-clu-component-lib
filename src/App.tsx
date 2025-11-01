c4d6db296ed5e28f197bde618fb1ecd0173ed812
Chaoran Lu <andyluloveusc@gmail.com>
2025-10-31 23:29:28 -0700

    Implement Button Functional Component

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,16 @@
 import React from 'react';
+import Button, {ButtonType, ButtonSize} from './components/Button/button.tsx';
 
 const App: React.FC = () => {
 
   return (
     <div className='App'>
       <header className='App-header'>
-        <h1>Welcome to React Component Library</h1>
+        <Button> This is a basic Button </Button>
+        <Button disabled> disabled </Button>
+        <Button btnType={ButtonType.Primary} size={ButtonSize.Large}> This is a Primary & Large Button </Button>
+        <Button btnType={ButtonType.Link} href="https://www.extron.com/"> Extron Link Button </Button>
       </header>
-      <p>Start coding with react 19</p>
     </div>
   );
 };
