716eeaf3f58f29af170f575e01c6ddadee19f0bf
Chaoran Lu <andyluloveusc@gmail.com>
2025-11-01 18:52:10 -0700

    add additional properties to the Button Component

--- src/App.tsx
+++ src/App.tsx
@@ -6,10 +6,12 @@ const App: React.FC = () => {
   return (
     <div className='App'>
       <header className='App-header'>
-        <Button> This is a basic Button </Button>
+        <Button onClick={(e) => {e.preventDefault(); alert("Happy Halloween!")}}>Test onClick</Button>
+        <Button autoFocus> This is a basic Button </Button>
         <Button disabled> disabled </Button>
         <Button btnType={ButtonType.Primary} size={ButtonSize.Large}> This is a Primary & Large Button </Button>
-        <Button btnType={ButtonType.Link} href="https://www.extron.com/"> Extron Link Button </Button>
+        <Button btnType={ButtonType.Danger} size={ButtonSize.Small}> This is a Danger & Small Button </Button>
+        <Button btnType={ButtonType.Link} href="https://www.extron.com/" target="_blank"> Extron Link Button </Button>
       </header>
     </div>
   );
