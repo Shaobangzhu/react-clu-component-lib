a7c434d9b0d1aeee603bc0aab8107681254dc853
Chaoran Lu <andyluloveusc@gmail.com>
2025-11-04 23:43:23 -0800

    add styles to Menu and MenuItem components

--- src/App.tsx
+++ src/App.tsx
@@ -3,21 +3,33 @@ import Menu from "./components/Menu/menu.tsx";
 import MenuItem from "./components/Menu/menuItem.tsx";
 
 const App: React.FC = () => {
-
-
   return (
     <div className="App">
       <header className="App-header">
-        <Menu defaultIndex={0} onSelect={(index) => {alert(index)}}>
-          <MenuItem index={1}>
-            cool link 1
-          </MenuItem>
+        <Menu
+          defaultIndex={0}
+          onSelect={(index) => {
+            alert(index);
+          }}
+        >
+          <MenuItem index={1}>cool link 1</MenuItem>
           <MenuItem index={2} disabled>
             cool link 2
           </MenuItem>
-          <MenuItem index={3}>
-            cool link 3
+          <MenuItem index={3}>cool link 3</MenuItem>
+        </Menu>
+        <Menu
+          mode="vertical"
+          defaultIndex={0}
+          onSelect={(index) => {
+            alert(index);
+          }}
+        >
+          <MenuItem index={1}>cool link 1</MenuItem>
+          <MenuItem index={2} disabled>
+            cool link 2
           </MenuItem>
+          <MenuItem index={3}>cool link 3</MenuItem>
         </Menu>
       </header>
     </div>
